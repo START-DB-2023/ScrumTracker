7b6100e09205904de972e332795cb31de42ef4bf
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2023-11-08 09:28:11 -0300

    :tada: init: Initial Setup with Spring Boot and Vite.

---
+++ scrum-tracker-back-end/scrum-tracker-api/src/main/java/com/db/scrumtrackerapi/ScrumTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.db.scrumtrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumTrackerApiApplication.class, args);
+	}
+
+}
---
+++ scrum-tracker-back-end/scrum-tracker-api/src/test/java/com/db/scrumtrackerapi/ScrumTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.scrumtrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
