fa87f418f570a16a7d0334853599b51f9c6b2e90
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2023-11-10 19:03:52 -0300

    :test_tube: test: Add CustomerRepositoryTest.
    Co-authored-by: ajuliamm <ajuliammelo@gmail.com>
    Co-authored-by: CaiquePrado <caiqueprado76@gmail.com>
    Co-authored-by: ChaiCaroline <chaiene_caroline@hotmail.com>

---
+++ scrum-tracker-back-end/scrum-tracker-api/src/test/java/com/db/scrumtrackerapi/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.db.scrumtrackerapi.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.db.scrumtrackerapi.models.Customer;
+import com.db.scrumtrackerapi.models.enums.Role;
+
+@DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CustomerRepositoryTest {
+    
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    private Customer expectedCustomer = new Customer("joao@email.com", "João", "Ninguém", "password", Role.ADMIN);  
+
+    @BeforeAll
+    void setup () {
+        customerRepository.save(expectedCustomer);
+    }
+
+    @Test
+    void assertFindCustomerByEmail() {
+        Customer actualCustomer = customerRepository.findByEmail("joao@email.com").get(0);
+        assertEquals(expectedCustomer, actualCustomer);
+    }
+}
