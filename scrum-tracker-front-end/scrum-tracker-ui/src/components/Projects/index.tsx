2f9bf14747469df633554c3ad931a2faa031eeec
ChaiCaroline <chaiene_caroline@hotmail.com>
2023-11-16 10:57:54 -0300

    git commit -m ":lipstick: table component creation and toast application >> > >> > >> Co-authored-by: VICTOR SILVA <victorhfsilva3@gmail.com> >> Co-authored-by: CAIQUE PEDRO <caiqueprado76@gmail.com> >> Co-authored-by: ANGELINA MELO <ajuliammelo@gmail.com> "

---
+++ scrum-tracker-front-end/scrum-tracker-ui/src/components/Projects/index.tsx
@@ -0,0 +1,12 @@
+import { ProjectsMain } from './styles';
+
+export default function Projects() {
+
+    return (
+        <>
+            <ProjectsMain>
+                <h1>Meus projetos</h1>
+            </ProjectsMain>
+        </>
+    )
+}
--- scrum-tracker-front-end/scrum-tracker-ui/src/main.tsx
+++ scrum-tracker-front-end/scrum-tracker-ui/src/main.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={3000}
+      closeButton={true}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable={false}
+      pauseOnHover
+      delay={1000}
+      limit={1}
+    />
   </React.StrictMode>,
 )
